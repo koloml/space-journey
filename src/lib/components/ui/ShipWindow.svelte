418c9e449f9ce6aa4b9d3faaa7af3d0668bc2437
KoloMl <thekolomoets@hotmail.com>
2023-04-16 03:54:33 +0400

    Added component for systems energy controls

--- src/lib/components/ui/ShipWindow.svelte
+++ src/lib/components/ui/ShipWindow.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import {onMount} from "svelte";
+    import SystemEnergyControl from "@/lib/components/ui/ship/SystemEnergyControl.svelte";
+    import type {SystemsEnergyInfo} from "@/lib/storage/SystemsEnergyStore";
+    import {systemsEnergyStore} from "@/lib/storage/SystemsEnergyStore";
+
+    let systemEnergyLevels: SystemsEnergyInfo;
 
     onMount(() => {
         // We're using the LittleJS engine which is always creates a canvas element in the root of the document. We
@@ -10,21 +15,45 @@
             glCanvas
         );
     });
+
+    systemsEnergyStore.subscribe(levels => {
+        systemEnergyLevels = levels;
+    });
 </script>
 
 <div id="ship-canvas">
-
+	<div class="bottom-left">
+		<div class="systems">
+			<SystemEnergyControl bind:value={systemEnergyLevels.farms} system="farm"></SystemEnergyControl>
+			<SystemEnergyControl bind:value={systemEnergyLevels.defence} system="defence"></SystemEnergyControl>
+			<SystemEnergyControl bind:value={systemEnergyLevels.generator} system="generator"></SystemEnergyControl>
+			<SystemEnergyControl bind:value={systemEnergyLevels.propulsion} system="thrusters"></SystemEnergyControl>
+		</div>
+	</div>
 </div>
 
 <style>
-	#ship-canvas {
-		position: relative;
-	}
+    #ship-canvas {
+        position: relative;
+		height: 100%;
+    }
 
-	#ship-canvas > :global(canvas) {
-		top: unset !important;
-		left: unset !important;
-		transform: unset !important;
-		height: unset !important;
-	}
+    #ship-canvas > :global(canvas) {
+        top: unset !important;
+        left: unset !important;
+        transform: unset !important;
+        height: unset !important;
+    }
+
+    .bottom-left {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+		z-index: 1;
+    }
+
+    .systems {
+        display: grid;
+        grid-row-gap: 4px;
+    }
 </style>
---
+++ src/lib/components/ui/ship/SystemEnergyControl.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+    import minusIconUrl from '@/assets/images/systems/minus.png';
+    import plusIconUrl from '@/assets/images/systems/plus.png';
+
+    export let value: number;
+    export let max = 2;
+    export let system: string;
+
+    let minusClassList, minusStyle;
+    let plusClassList, plusStyle;
+
+    $: minusClassList = value > 0 ? 'minus' : 'minus disabled';
+    $: plusClassList = value < max ? 'plus' : 'plus disabled';
+    $: minusStyle = `--icon-url: url(${minusIconUrl})`;
+    $: plusStyle = `--icon-url: url(${plusIconUrl})`;
+
+    let systemIconUrl = new URL(`/src/assets/images/systems/${system}.png`, import.meta.url);
+</script>
+
+<div class="control">
+	<div class="{minusClassList}" style="{minusStyle}" on:click="{() => (value = Math.max(--value, 0))}"></div>
+	<div class="state">
+		<div class="full" style="--width: {max}"></div>
+		<div class="current" style="--width: {value}"></div>
+		<img src="{systemIconUrl}" alt="{system}" class="{value === 0 ? 'disabled' : ''}">
+	</div>
+	<div class="{plusClassList}" style="{plusStyle}" on:click="{() => (value = Math.min(++value, max))}"></div>
+</div>
+
+<style>
+    .control {
+        display: flex;
+        align-items: flex-end;
+    }
+
+    .state {
+        height: 3px;
+        position: relative;
+        margin-left: 3px;
+    }
+
+    .full {
+        background-image: repeating-linear-gradient(
+                to right,
+                var(--color-secondary) 0, var(--color-secondary) 2px,
+                var(--color-background) 2px, var(--color-background) 3px);
+        width: calc(3px * (var(--width) + 1) - 1px);
+    }
+
+    .current {
+        background-image: repeating-linear-gradient(
+                to right,
+                var(--color-primary) 0, var(--color-primary) 2px,
+                var(--color-background) 2px, var(--color-background) 3px);
+        width: calc(3px * (var(--width) + 1) - 1px);
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    .full, .current {
+        background-repeat: repeat-x;
+        height: 3px;
+    }
+
+    .state img {
+        position: absolute;
+        bottom: 0;
+        left: -2px;
+        width: 7px;
+        height: 7px;
+        object-fit: cover;
+        object-position: right;
+        margin: -1px;
+    }
+	
+	.state img.disabled {
+		object-position: left;
+	}
+
+    .minus, .plus {
+        --icon-url: unset;
+
+        width: 5px;
+        height: 5px;
+        background-image: var(--icon-url);
+        background-position-x: 0;
+        cursor: pointer;
+        margin: -1px 0;
+    }
+
+    .minus:hover, .plus:hover {
+        background-position-x: 5px;
+    }
+
+    .minus.disabled, .plus.disabled {
+        opacity: 0;
+    }
+</style>
