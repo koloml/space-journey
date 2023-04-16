729d435229a5a5a20aca417c23ef8defc8a5ddbb
KoloMl <thekolomoets@hotmail.com>
2023-04-16 23:31:39 +0400

    Updated journey progress bar to use planets icons

--- src/lib/components/ui/resources/JourneyProgress.svelte
+++ src/lib/components/ui/resources/JourneyProgress.svelte
@@ -1,48 +1,73 @@
 <script lang="ts">
-	import progressArrowUrl from "@/assets/images/journey-progress/progress-arrow.png";
+    import progressArrowUrl from "@/assets/images/journey-progress/progress-arrow.png";
+    import planetIconUrl from "@/assets/images/journey-progress/planet.png";
 
     export let value = 0;
     export let max = 0;
 
-    let progress = value / max;
-    let progressPercent = Math.round(progress * 100);
+    $: progress = value / max;
+    $: progressPercent = Math.round(progress * 100);
 </script>
 
 <div class="progress">
-	<div class="full"></div>
-	<div class="bar" style="width: {progressPercent}%">
-		<img src="{progressArrowUrl}" alt="Progress">
+	<img src="{planetIconUrl}" alt="Departure" class="planet start">
+	<div class="bar">
+		<div class="full"></div>
+		<div class="current" style="width: {progressPercent}%">
+			<img src="{progressArrowUrl}" alt="Progress">
+		</div>
 	</div>
+	<img src="{planetIconUrl}" alt="Destination" class="planet target">
 </div>
 
 <style>
     .progress {
-		position: relative;
-		height: 5px;
-		grid-column: 1 / end;
-		display: flex;
-		padding: 1px;
-		border-left: 1px solid var(--color-primary);
-		width: 100px;
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        grid-gap: 1px;
+        align-items: center;
+        grid-column: 1 / end;
     }
 
-	.full {
-		position: absolute;
-		height: 1px;
-		background: var(--color-secondary);
-		width: 100%;
-		top: 2px;
-		z-index: -1;
-	}
+    .bar {
+        position: relative;
+        height: 5px;
+        display: flex;
+        padding: 1px;
+        border-left: 1px solid var(--color-primary);
+    }
 
-	.bar {
-		background: var(--color-primary);
-		position: relative;
-	}
+    .full {
+        position: absolute;
+        height: 1px;
+        background: var(--color-secondary);
+        top: 2px;
+        left: 3px;
+        right: 0;
+        z-index: -1;
+    }
 
-	.bar img {
-		position: absolute;
-		right: -3px;
-		top: -2px;
-	}
+    .current {
+        background: var(--color-primary);
+        position: relative;
+    }
+
+    .current img {
+        position: absolute;
+        right: -3px;
+        top: -2px;
+    }
+
+    .planet {
+        width: 9px;
+        height: 9px;
+        object-fit: cover;
+        object-position: left;
+    }
+
+    .planet.target {
+        object-position: right;
+    }
+
+
 </style>
