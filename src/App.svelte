66a1ac558a2d2be26468a069390d861a89c3386a
KoloMl <thekolomoets@hotmail.com>
2023-04-16 19:05:14 +0400

    Block any resize attempts

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,23 @@
     import ResourcesWindow from "@/lib/components/ui/ResourcesWindow.svelte";
     import ShipWindow from "@/lib/components/ui/ShipWindow.svelte";
     import DecisionsWindow from "@/lib/components/ui/DecisionsWindow.svelte";
+    import {onMount} from "svelte";
+
+    onMount(() => {
+        // Disable the zoom by Ctrl+Scroll
+        document.addEventListener('wheel', (e) => {
+            if (e.ctrlKey) {
+                e.preventDefault();
+            }
+        }, {passive: false});
+
+        // Disable the zoom by Ctrl+Plus/Minus
+        document.addEventListener('keydown', (e) => {
+            if (e.ctrlKey && (e.key === '+' || e.key === '=' || e.key === '-')) {
+                e.preventDefault();
+            }
+        });
+    });
 </script>
 
 <GameContainer>
