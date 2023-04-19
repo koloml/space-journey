b3868e1b32a1dea1a17ce3bff6c1ba2708199701
KoloMl <thekolomoets@hotmail.com>
2023-04-20 02:18:44 +0400

    Added food to the storage

--- src/lib/storage/ResourcesStore.ts
+++ src/lib/storage/ResourcesStore.ts
@@ -4,10 +4,12 @@ export interface ResourcesInfo {
     integrity: number;
     crew: number;
     matter: number;
+    food: number;
 }
 
 export const resourcesStore = writable<ResourcesInfo>({
     integrity: 100,
     crew: 100,
     matter: 100,
+    food: 100
 });
