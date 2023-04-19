b50908f5d269d534ccfbbe92abffb47d62b11cb4
nenefurka <nenefurka@gmail.com>
2023-04-20 03:20:44 +0500

    Food icon, first event, redrawn farm icon

--- src/lib/game/ship/events/EventsCollection.ts
+++ src/lib/game/ship/events/EventsCollection.ts
@@ -1,3 +1,30 @@
 import type {GameEventInit} from "@/lib/game/ship/events/decisions/GameEvent";
 
-export const eventsCollection: GameEventInit[] = [];
+export const eventsCollection: GameEventInit[] = [
+    {
+        text: "Epidemic on the ship!",
+        choices: [
+            {
+                text: "ignore",
+                run: game => {
+                    game.resources.update(value => {
+                        value.crew -= rand(5, 10)
+                        return value
+                    })
+                    game.logger.log("ignore")
+                }
+            },
+            {
+                text: "find antidote",
+                run: game => {
+                    game.resources.update(value => {
+                        value.crew -= rand(0, 5)
+                        value.matter -= 5
+                        return value
+                    })
+                    game.logger.log("antidote")
+                }
+            }
+        ]
+    }
+];
