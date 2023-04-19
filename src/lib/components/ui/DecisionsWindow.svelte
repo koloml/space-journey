93920c10d298f6fe4ba2d6ad3d532059aba4a4a6
KoloMl <thekolomoets@hotmail.com>
2023-04-20 01:03:57 +0400

    Dummy decision and decision choices components

--- src/lib/components/ui/DecisionsWindow.svelte
+++ src/lib/components/ui/DecisionsWindow.svelte
@@ -1,3 +1,8 @@
-<div>
-	Decisions
+<div class="decisions">
 </div>
+
+<style>
+	.decisions {
+		display: grid;
+	}
+</style>
---
+++ src/lib/components/ui/decisions/DecisionBlock.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+</script>
+
+<div class="decision">
+	<div class="text"></div>
+	<div class="choices"></div>
+</div>
+
+<style></style>
