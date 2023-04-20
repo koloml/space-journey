ccb28c6ec1901c98020e80620a0765f0b0c4955a
KoloMl <thekolomoets@hotmail.com>
2023-04-20 23:47:34 +0400

    Toggle the strength of thrusters exhaust depending on the thrusters module energy

--- src/lib/game/ship/modules/ThrustersModule.ts
+++ src/lib/game/ship/modules/ThrustersModule.ts
@@ -1,6 +1,7 @@
 import BaseShipModule from "@/lib/game/ship/base/BaseShipModule";
 import ExhaustEmitter from "@/lib/game/ship/modules/thrusters/ExhaustEmitter";
 import SecondaryExhaustEmitter from "@/lib/game/ship/modules/thrusters/SecondaryExhaustEmitter";
+import type {SystemsStatusInfo} from "@/lib/storage/SystemsStatusStore";
 
 export default class ThrustersModule extends BaseShipModule {
     tileIndex = 48;
@@ -35,6 +36,24 @@ export default class ThrustersModule extends BaseShipModule {
                 ),
                 PI
             );
+        });
+
+        this.game.systems.subscribe(systems => this._onSystemsUpdated(systems));
+    }
+
+    /**
+     * Update the exhaust emitters based on the current system status.
+     * @param systems Systems status.
+     * @private
+     */
+    private _onSystemsUpdated(systems: SystemsStatusInfo) {
+        console.log('Updated', systems);
+
+        const thrusters = systems.thrusters;
+
+        this._exhaustEmitters.forEach(emitter => {
+            emitter.scale(thrusters.energy / thrusters.maxEnergy);
+            emitter.toggle(thrusters.active);
         })
     }
 
