15a600e7e4e8ad2e56e6a563c64b709e1d8be7ee
KoloMl <thekolomoets@hotmail.com>
2023-04-22 18:39:30 +0400

    Storing the resources changes if available

--- src/lib/storage/LogsStore.ts
+++ src/lib/storage/LogsStore.ts
@@ -1,8 +1,10 @@
 import {writable} from "svelte/store";
+import type {ResourcesInfo} from "@/lib/storage/ResourcesStore";
 
 export interface LogEntry {
     text: string;
     timestamp: string;
+    resources?: Partial<ResourcesInfo>;
 }
 
 export const logsStore = writable<LogEntry[]>([]);
