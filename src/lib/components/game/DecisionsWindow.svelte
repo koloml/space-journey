11da38799a4cf2834d8ec18a4060af068c7cfb6e
KoloMl <thekolomoets@hotmail.com>
2023-04-22 19:42:23 +0400

    Render the event & event choices

--- src/lib/components/game/DecisionsWindow.svelte
+++ src/lib/components/game/DecisionsWindow.svelte
@@ -1,8 +1,21 @@
-<div class="decisions">
-</div>
+<script lang="ts">
+    import {activeDecisionStore} from "@/lib/storage/ActiveDecisionStore";
+    import type GameEvent from "@/lib/game/events/entities/GameEvent";
+    import DecisionBlock from "@/lib/components/game/decisions/DecisionBlock.svelte";
+
+    let decision: GameEvent | null = null;
+
+    activeDecisionStore.subscribe(store => {
+        decision = store.decision;
+    });
+</script>
+
+{#if decision}
+	<DecisionBlock bind:event={decision}></DecisionBlock>
+{/if}
 
 <style>
-	.decisions {
-		display: grid;
-	}
+    .decisions {
+        display: grid;
+    }
 </style>
--- src/lib/components/game/decisions/DecisionBlock.svelte
+++ src/lib/components/game/decisions/DecisionBlock.svelte
@@ -1,10 +1,70 @@
 <script lang="ts">
+    import type GameEvent from "@/lib/game/events/entities/GameEvent";
 
+    export let event: GameEvent;
 </script>
 
-<div class="decision">
-	<div class="text"></div>
-	<div class="choices"></div>
-</div>
+{#if event}
+	<div class="decision">
+		<div class="text">{event.text}</div>
+		<div class="choices">
+			{#each event.choices as choice}
+				<div class="choice">{choice.text}</div>
+			{/each}
+		</div>
+	</div>
+{/if}
 
-<style></style>
+<style>
+    .decision {
+        font-family: LazyFox7, serif;
+        font-size: 8px;
+        line-height: 7px;
+        margin: 1px;
+		display: flex;
+		flex-direction: column;
+		gap: 2px;
+    }
+
+	.choices {
+		display: flex;
+		gap: 2px;
+		flex-wrap: wrap;
+	}
+
+	.choice {
+		flex-grow: 1;
+	}
+
+    .text, .choice {
+        padding: 3px 2px 2px;
+        position: relative;
+        text-align: center;
+		border-color: var(--color-secondary);
+    }
+
+	.text {
+		border-color: var(--color-primary);
+	}
+
+    .text::before, .text::after, .choice::before, .choice::after {
+        content: "";
+        display: block;
+        border-color: inherit;
+        border-style: solid;
+        position: absolute;
+        left: 0;
+        right: 0;
+        height: 1px;
+    }
+
+    .text::before, .choice::before {
+        top: 0;
+        border-width: 1px 1px 0 1px;
+    }
+
+    .text::after, .choice::after {
+        bottom: 0;
+        border-width: 0 1px 1px 1px;
+    }
+</style>
