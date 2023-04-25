e2c7c184f9108a3cc46c460f2332926145548d8a
KoloMl <thekolomoets@hotmail.com>
2023-04-26 03:30:37 +0400

    Handling crew & food changes

--- src/lib/game/controllers/ResourcesController.ts
+++ src/lib/game/controllers/ResourcesController.ts
@@ -9,6 +9,8 @@ export default class ResourcesController extends BaseController {
 
     onTick() {
         this._generateEnergy();
+        this._handleFoodChanges();
+        this._handleCrewChanges();
     }
 
     /**
@@ -35,6 +37,69 @@ export default class ResourcesController extends BaseController {
 
                 store.energyProgress = 0;
                 store.totalEnergy += 1;
+            }
+
+            return store;
+        });
+    }
+
+    /**
+     * Handle food consumption by the crew and new food generation by the farms.
+     * @private
+     */
+    private _handleFoodChanges() {
+        if (this._game.ticksPassed % 5 !== 0) {
+            return;
+        }
+
+        const farms = this._game.systems.get('farms');
+        const amountOfCrew = this._game.resources.get('crew');
+        const foodConsumption = Math.round(amountOfCrew / 50);
+        const foodProduction = +farms.active * farms.energy * 2;
+
+        this._game.resources.store.update(store => {
+            store.food += foodProduction - foodConsumption;
+            store.food = Math.max(0, store.food);
+
+            if (store.food > 100) {
+                store.food -= 1;
+            }
+
+            return store;
+        });
+    }
+
+    /**
+     * Handle the crew changes onboard the ship. If there is not enough food, the crew will start to die.
+     * If there is less the 2/3 of the food reserves, the crew will start to deteriorate.
+     * @private
+     */
+    private _handleCrewChanges() {
+        if (this._game.ticksPassed % 5 !== 0) {
+            return;
+        }
+
+        const amountOfCrew = this._game.resources.get('crew');
+
+        if (amountOfCrew <= 0) {
+            return;
+        }
+
+        const food = this._game.resources.get('food');
+        const minimumFoodRequirements = Math.round(amountOfCrew * .3);
+
+        this._game.resources.store.update(store => {
+            if (food < minimumFoodRequirements) {
+                store.crew -= 1;
+                return store;
+            }
+
+            if (this._game.ticksPassed % 15 === 0 && store.crew < 100) {
+                store.crew += 1;
+            }
+
+            if (store.crew > 100 && this._game.ticksPassed % 30 === 0) {
+                store.crew -= 1;
             }
 
             return store;
